7fb454e680dd32d9bd49c34a21aa0dc24e90ebfc
Mandeep Singh <smandeep651@gmail.com>
2025-02-19 13:40:19 -0500

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import AdminLayout from "./components/layouts/AdminLayout";
-import { QueryClient ,QueryClientProvider} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
-
-const queryClient =new QueryClient()
-
+const queryClient = new QueryClient()
 const App = () => {
-  return 
-
-  <QueryClientProvider client ={queryClient}>
+  return (
+  <QueryClientProvider client = {queryClient}>
     <AdminLayout />
   </QueryClientProvider>
- 
+  );
+
 };
 
 export default App
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,39 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
-import BooksTable from "../components/BooksTable";
-
+import { Outlet } from "react-router-dom";
+import BooksTable from "../components/books/BooksTable"
 
 const Books = () => {
 
-  const {isPending,error,data:books}=useQuery({
-    queryKey:['booksData'],
-    queryFn:async ()=>{
-      console.log('Fetching Data')
-      const response=    await fetch('http://localhost:3000/books')
-      return response.json()
-    }
-  })
-  
-  if(isPending)return <div>Loading...</div>
+    const { isPending, error, data: books } = useQuery({
+        queryKey: ['booksData'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:3000/books');
+            return response.json() //returns a promise of our data
+        },
+        staleTime: Infinity //cache the data & store it locally
+    }) 
 
-  if (error)return <div>{`An error has occured + ${error.message}`}</div>
-
-
-
+    if(error) return <div>{`An error has occured: ${error.message}`}</div>;
 
     return (
       <div>
+        <Outlet />
         <h1 className="text-2xl font-bold">Books</h1>
-       
         {
-        isPending?
-        <div> Loading...</div>
-          :
-         <BooksTable books ={data}/>
-       
-       
+            isPending ?
+            <div>Loading...</div> 
+            :
+            <BooksTable books = {books} />
         }
-      </div>
+    </div>
     );
-  };
-  export default Books;
+};
+
+export default Books;
   
